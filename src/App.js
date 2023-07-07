c51c7d887c724b92c421fd3e4366c02903bc530c
Bengo-Concepts <Julienbenghozi@gmail.com>
2023-07-07 14:30:57 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
+import CurrencyDropdown from './components/CurrencyDropdown';
 import { AppProvider } from './context/AppContext';
-import CurrencyDropdown from './components/CurrencyDropdown'
-
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        { // Budget component
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
-                    </div>}        
-
-                    { //Currency component
-                    <div className='col-sm'>
-                        <CurrencyDropdown />
-                    </div>}     
-
-                        {//Remaining component
+                    </div>
                     <div className='col-sm'>
                         <Remaining />
-                    </div>}        
-
-                        {  //ExpenseTotal component
+                    </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
-                    </div>}        
-                       
-                        {  //ExpenseList component
+                    </div>
+                    <div className='col-sm'>
+                        <CurrencyDropdown />
+                    </div>
+                </div>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <ExpenseList />
-                    </div>}        
-                                
-
-                        {  //AllocationForm component
+                    </div>
+                </div>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm />
-                    </div>}  
-
+                    </div>
                 </div>
             </div>
         </AppProvider>
     );
 };
+
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,57 +1,57 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import {FaMinusCircle} from 'react-icons/fa';
+import {FaPlusCircle} from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
-    const { dispatch, currency } = useContext(AppContext);
-    const currencySymbol = currency === '£' ? '£' : currency === '€' ? '€' : currency === '₹' ? '₹' : '$';
+  const { dispatch, currency } = useContext(AppContext);
+  const currencySymbol = currency === '£' ? '£' : currency === '€' ? '€' : currency === '₹' ? '₹' : '$';
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense,
-        });
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: -10,
     };
 
-    const decreaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: -10,
-        };
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense,
-        });
-    };
-
-    return (
-        <tr>
-            <td>{props.name}</td>
-            <td>{currencySymbol}{props.cost}</td>
-            <td>
-                <button onClick={(event) => increaseAllocation(props.name)}>+</button>
-            </td>
-            <td>
-                <button onClick={(event) => decreaseAllocation(props.name)}>-</button>
-            </td>
-            <td>
-                <TiDelete size="1.5em" onClick={handleDeleteExpense} />
-            </td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{currencySymbol}{props.cost}</td>
+      <td><FaPlusCircle size='2.2em' color="green" onClick={(event) => decreaseAllocation(props.name)}></FaPlusCircle></td>
+      <td><FaMinusCircle size='2.2em' color="red" onClick={(event) => increaseAllocation(props.name)}></FaMinusCircle></td>
+      <td>
+        <TiDelete size="1.5em" onClick={handleDeleteExpense} />
+      </td>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
+
+
